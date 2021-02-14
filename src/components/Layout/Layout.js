9a48959e91dd582f5c1aab4e299c621ffda667bb
arturo.calvo <arturocalvodevesa@gmail.com>
2021-02-14 08:12:26 +0100

    Initial deploy

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const layout = (props) => (
+    <React.Fragment>
+        <div>Toolbar</div>
+        <div>Main screen</div>
+    </React.Fragment>
+);
+
+export default layout;
