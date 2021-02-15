797682ffd48954e474a3a62bf9d907c285abcfe2
arturo.calvo <arturocalvodevesa@gmail.com>
2021-02-15 16:30:47 +0100

    Added end screen

---
+++ src/components/MainScreen/EndScreen/EndScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './EndScreen.module.css';
+
+const endScreen = (props) => (
+    <div className={classes.EndScreen}>
+        <div>
+            <h2>Game Over!</h2>
+            <h3>Level: <strong>{props.level}</strong></h3>
+            <h3>Score: <strong>{props.score}</strong></h3>
+        </div>
+        <div>
+            <button 
+                className={classes.RestartButton} 
+                onClick={props.restartGame}>Start Game</button>
+        </div>
+    </div>
+);
+
+export default endScreen;
