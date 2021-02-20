885abef788050df18a31a02296f5b05d58c09906
arturo.calvo <arturocalvodevesa@gmail.com>
2021-02-20 09:36:42 +0100

    How does it work texts

--- src/components/MainScreen/StartScreen/StartScreen.js
+++ src/components/MainScreen/StartScreen/StartScreen.js
@@ -9,8 +9,9 @@ const style = {
 
 const startScreen = (props) => (
     <div className={classes.StartScreen}>
-         <div className={classes.HeaderText}><strong>Kobadoo</strong> is a free memory game that trains your brain using funny emojis.</div>
-
+         <div className={classes.HeaderText}>
+             <strong>Kobadoo</strong> is a free memory game that trains your brain using funny emojis.
+         </div>
         <div>
             <button 
                 className={classes.StartButton} 
@@ -18,9 +19,16 @@ const startScreen = (props) => (
         </div>
 
         <div className={classes.BenefitsText}>
+            <h4>How does it work?</h4>
+            <p>Click on <i>Start Game</i> and <strong>memorize the emojis</strong> that are displayed.
+               Then <strong>select them in the right order</strong> to pass to the next level. It gets more challenging over time! 
+               <strong> 100% free</strong>, no download or sign up required. <strong>Play on mobile</strong> for the best experience.</p>
+        </div>
+
+        <div className={classes.BenefitsText}>
             <img className={classes.BrainImage} src='/images/brain.png' />
             <h4>Why is Kobadoo good for your mind?</h4>
-            <p>Playing memory games help sharpen certain brain functions that tend to wane with age, such as <b>attention</b> to detail, <b>concentration</b>, <b>reaction time</b>, <b>decision making</b>, and <b>short-term memory</b>.</p>
+            <p>Playing memory games help sharpen certain brain functions that tend to wane with age, such as <b>attention</b> to detail, <b>concentration</b>, <b>reaction time</b>, <b>decision making</b>, and <b>short-term memory</b>. <i>Kobadoo</i> has been created in <strong>cooperation with neuropsychologists</strong> in Norway.</p>
         </div>
 
         { /*
