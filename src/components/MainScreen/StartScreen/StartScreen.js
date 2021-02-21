4ba8ce9f2ee1a4b0b8616e17a019cdb4e9803ea4
arturo.calvo <arturocalvodevesa@gmail.com>
2021-02-21 17:00:35 +0100

    Added new text on working memory

--- src/components/MainScreen/StartScreen/StartScreen.js
+++ src/components/MainScreen/StartScreen/StartScreen.js
@@ -22,14 +22,20 @@ const startScreen = (props) => (
         <div className={classes.BenefitsText}>
             <h4>How does it work?</h4>
             <p>Click on <i>Start Game</i> and <strong>memorize the emojis</strong> that are displayed.
-               Then <strong>select them in the right order</strong> to pass to the next level. It gets more challenging over time! 
+               Then <strong>select them in the right order</strong> to be able to reach the next level. It gets more challenging over time! 
                <strong> 100% free</strong>, no download or sign up required. <strong>Play on mobile</strong> for the best experience.</p>
         </div>
 
         <div className={classes.BenefitsText}>
             <img className={classes.BrainImage} src='/images/brain.png' />
             <h4>Why is Kobadoo good for your mind?</h4>
-            <p>Playing memory games help sharpen certain brain functions that tend to wane with age, such as <b>attention</b> to detail, <b>concentration</b>, <b>reaction time</b>, <b>decision making</b>, and <b>short-term memory</b>. <i>Kobadoo</i> has been created in <strong>cooperation with neuropsychologists</strong> in Norway.</p>
+            <p>Playing memory games help sharpen certain brain functions that tend to wane with age, such as <strong>attention</strong> to detail, <strong>concentration</strong>, <strong>reaction time</strong>, <strong>decision making</strong>, and <strong>working memory</strong>. <i>Kobadoo</i> has been created in <strong>cooperation with a Norwegian neuropsychologist</strong>.</p>
+        </div>
+
+        <div className={classes.BenefitsText}>
+            <img className={classes.BrainImage} src='/images/thinking.png' />
+            <h4>What is the <i>working memory</i> and why should you train it?</h4>
+            <p>Working memory is a form of short-term memory, where you temporarily hold information in your mind while processing it. When you play <i>Kobadoo</i> you are actively using your working memory. There are several scientific studies that suggest training of working memory has <strong>beneficial effects</strong> and can <strong>increase your cognitive functions</strong>. So <strong>have fun</strong> while you also train your working memory and attention!</p>
         </div>
 
         { /*
