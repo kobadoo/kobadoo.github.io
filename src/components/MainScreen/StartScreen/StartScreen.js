a3921bc4665185a6911946a54d549d4065dad1cc
arturo.calvo <arturocalvodevesa@gmail.com>
2021-02-23 21:32:35 +0100

    Fixed styles

--- src/components/MainScreen/StartScreen/StartScreen.js
+++ src/components/MainScreen/StartScreen/StartScreen.js
@@ -24,7 +24,7 @@ const startScreen = (props) => (
 
         <div className={classes.BenefitsText}>
             <img className={classes.StartImage} src={MonkeyImg} />
-            <h4>How does it work?</h4>
+            <h3>How does it work?</h3>
             <p>Click on <i>Start Game</i> and <strong>memorize the emojis</strong> that are displayed.
                Then <strong>select them in the right order</strong> to be able to reach the next level. It gets more challenging over time! 
                <strong> 100% free</strong>, no download or sign up required. <strong>Play on mobile</strong> for the best experience.</p>
@@ -32,13 +32,13 @@ const startScreen = (props) => (
 
         <div className={classes.BenefitsText}>
             <img className={classes.StartImage} src={BrainImg} />
-            <h4>Why is Kobadoo good for your mind?</h4>
+            <h3>Why is Kobadoo good for your mind?</h3>
             <p>Playing memory games help sharpen certain brain functions that tend to wane with age, such as <strong>attention</strong> to detail, <strong>concentration</strong>, <strong>reaction time</strong>, <strong>decision making</strong>, and <strong>working memory</strong>. <i>Kobadoo</i> has been created in <strong>cooperation with a specialist in medical neuropsychology</strong>.</p>
         </div>
 
         <div className={classes.BenefitsText}>
             <img className={classes.StartImage} src={TrainImg} />
-            <h4>What is the <i>working memory</i> and why should you train it?</h4>
+            <h3>What is the <i>working memory</i> and why should you train it?</h3>
             <p>Working memory is a form of short-term memory, where you temporarily hold information in your mind while processing it. When you play <i>Kobadoo</i> you are actively using your working memory. There are several scientific studies that suggest training of working memory has <strong>beneficial effects</strong> and can <strong>increase your cognitive functions</strong>. So <strong>have fun</strong> while you also train your working memory and attention!</p>
         </div>
 
