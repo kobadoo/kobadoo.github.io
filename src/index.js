19962481197a59157d35d46f4c0eb68b15f3b205
kobadoo <kobadooapp@gmail.com>
2021-02-28 09:33:33 +0100

    Added Redux DevTools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <React.StrictMode>
