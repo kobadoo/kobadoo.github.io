7c93678f5a268c9a6808793be4d777f801f9e305
kobadoo <kobadooapp@gmail.com>
2021-03-01 22:07:05 +0100

    Refactor reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../utils/objectUpdater';
 
 const initialState = {
     level: 1,
@@ -9,53 +10,78 @@ const initialState = {
 
 const POINTS_PER_COMPLETED_LEVEL = 100;
 
+const increaseScore = (state, action) => {
+    const updatedState = { 
+        score: state.score + action.addedScore 
+    };
+    return updateObject(state, updatedState);
+};
+
+const passLevel = (state, action) => {
+    const updatedState = {
+        level: state.level + 1,
+        score: state.score + POINTS_PER_COMPLETED_LEVEL,
+        hasLevelStarted: false
+    };
+    return updateObject(state, updatedState);
+};
+
+const startLevel = (state, action) => {
+    const updatedState = {
+        hasLevelStarted: true
+    };
+    return updateObject(state, updatedState);
+};
+
+const endGame = (state, action) => {
+    const updatedState = { 
+        hasGameEnded: true 
+    };
+    return updateObject(state, updatedState);
+};
+
+const restartGame = (state, action) => {
+    const updatedState = { 
+        level: 1,
+        score: 0,
+        hasLevelStarted: true,
+        hasGameEnded: false
+    };
+    return updateObject(state, updatedState);
+};
+
+const abortGame = (state, action) => {
+    const updatedState = { 
+        level: 1,
+        score: 0,
+        hasLevelStarted: false,
+        hasGameEnded: false 
+    };
+    return updateObject(state, updatedState);
+};
+
+
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
 
         case actionTypes.INCREASE_SCORE:
-            return {
-                ...state,
-                score: state.score + action.addedScore
-            };
+            return increaseScore(state, action);
 
         case actionTypes.PASS_LEVEL:
-            return {
-                ...state,
-                level: state.level + 1,
-                score: state.score + POINTS_PER_COMPLETED_LEVEL,
-                hasLevelStarted: false
-            };
+            return passLevel(state, action);
 
         case actionTypes.START_LEVEL:
-            return {
-                ...state,
-                hasLevelStarted: true
-            };
+            return startLevel(state, action);
         
         case actionTypes.END_GAME:
-            return {
-                ...state,
-                hasGameEnded: true
-            };
+            return endGame(state, action);
         
         case actionTypes.RESTART_GAME:
-            return {
-                ...state,
-                level: 1,
-                score: 0,
-                hasLevelStarted: true,
-                hasGameEnded: false
-            };
+            return restartGame(state, action);
         
         case actionTypes.ABORT_GAME:
-            return {
-                ...state,
-                level: 1,
-                score: 0,
-                hasLevelStarted: false,
-                hasGameEnded: false
-            };
+            return abortGame(state, action);
 
         default:
             return state;
