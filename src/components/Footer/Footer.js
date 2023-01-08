6ff8d39ce0f17aafeccf06617a17e0a014805a78
Arturo Calvo <kobadooapp@gmail.com>
2023-01-08 23:02:03 +0100

    Added Youtube icon on footer
    	modified:   src/components/Footer/Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faBlogger, faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import classes from './Footer.module.css';
@@ -10,7 +10,8 @@ const footer = () => (
         <div className={classes.FooterItem}><a href="mailto: hello@kobadoo.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></div>
         <div className={classes.FooterItem}><a href="https://twitter.com/kobadooApp" target="_new"><FontAwesomeIcon icon={faTwitter} size="2x" /></a></div>
         <div className={classes.FooterItem}><a href="https://facebook.com/kobadooApp" target="_new"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://blog.kobadoo.com" target="_new">Blog</a></div>
+        <div className={classes.FooterItem}><a href="https://www.youtube.com/channel/UCGH5U9vx9IOudz4yujsghPA" target="_new"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://blog.kobadoo.com" target="_new"><FontAwesomeIcon icon={faBlogger} size="2x" /></a></div>
         <div className={classes.FooterItem}><Link to="/terms">Terms</Link></div>
         <div className={classes.FooterItem}><Link to="/privacy">Privacy</Link></div>
         <div className={classes.FooterItem}><Link to="/credits">Credits</Link></div>
