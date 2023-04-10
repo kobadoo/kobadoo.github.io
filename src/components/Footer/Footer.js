0cc847862c57b3ceb1530acbe9babad9dc683e05
Arturo Calvo <kobadooapp@gmail.com>
2023-04-11 00:50:44 +0200

    Switched _new to _blank

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,10 +8,10 @@ import classes from './Footer.module.css';
 const footer = () => (
     <div className={classes.Footer}>
         <div className={classes.FooterItem}><a href="mailto: hello@kobadoo.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://twitter.com/kobadooApp" target="_new"><FontAwesomeIcon icon={faTwitter} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://facebook.com/kobadooApp" target="_new"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://www.youtube.com/@kobadoo/videos" target="_new"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://blog.kobadoo.com" target="_new"><FontAwesomeIcon icon={faBlogger} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://twitter.com/kobadooApp" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://facebook.com/kobadooApp" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://www.youtube.com/@kobadoo/videos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://blog.kobadoo.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faBlogger} size="2x" /></a></div>
         <div className={classes.FooterItem}><Link to="/terms">Terms</Link></div>
         <div className={classes.FooterItem}><Link to="/privacy">Privacy</Link></div>
         <div className={classes.FooterItem}><Link to="/credits">Credits</Link></div>
