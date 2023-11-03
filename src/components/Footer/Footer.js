0fe4e8e020cd45462072c692e552d0f0837a1dbd
Arturo Calvo <kobadooapp@gmail.com>
2023-11-03 09:50:11 +0100

    Switched Twitter X logo. Ads.txt update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBlogger, faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faBlogger, faFacebook, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import classes from './Footer.module.css';
@@ -8,7 +8,7 @@ import classes from './Footer.module.css';
 const footer = () => (
     <div className={classes.Footer}>
         <div className={classes.FooterItem}><a href="mailto: hello@kobadoo.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></div>
-        <div className={classes.FooterItem}><a href="https://twitter.com/kobadooApp" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" /></a></div>
+        <div className={classes.FooterItem}><a href="https://x.com/kobadooApp" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faXTwitter} size="2x" /></a></div>
         <div className={classes.FooterItem}><a href="https://facebook.com/kobadooApp" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></div>
         <div className={classes.FooterItem}><a href="https://www.youtube.com/@kobadoo/videos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></div>
         <div className={classes.FooterItem}><a href="https://blog.kobadoo.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faBlogger} size="2x" /></a></div>
